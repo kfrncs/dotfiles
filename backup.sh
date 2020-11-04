8e8ce1a05156cd7f7b0637fa6b618370eeafb0d7
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-11-04 23:58:25 +0000

    add backup script

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+echo "copying ~/.zshrc"
+cp ~/.zshrc zshrc
+echo "copying ~/.tmux.conf"
+cp ~/.tmux.conf tmux.conf
+echo "copying ~/.config/nvim/init.vim" 
+cp ~/.config/nvim/init.vim ./config/nvim/
+echo "copying ~/.config/termite/config"
+cp ~/.config/termite/config ./config/termite/
+
+
