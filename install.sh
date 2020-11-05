1cc0777f6d017a099a4610641baea6e198490ed6
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-11-05 21:22:55 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# TODO
+# list of Pacman packages to install:
+# wget tree git nvim tmux 
+
+# Install separately:
+# Rust
+
+echo "installing z.sh jumparound"
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+mv z.sh ~
+
+
