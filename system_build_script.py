e6d98f27219121eaee3f7a48a63bbebe9d6c3e1c
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-11-29 11:35:38 -0500

    start system_build_script.py

---
+++ system_build_script.py
@@ -0,0 +1,20 @@
+# ARE YOU SURE YOU WANT TO DO THIS? Y/N
+print("WARNING:")
+print("THIS SCRIPT IS FOR BUILDING A NEW SYSTEM. ARE YOU SURE?")
+
+# install fish shell
+# $ curl -L https://get.oh-my.fish | fish
+
+# install fisher, package manager for fish
+# $ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# fisher plugs
+
+# pisces
+# $ fisher install laughedelic/pisces
+
+# z fish
+# $ fisher install jethrokuan/z
+
+# gitnow (git helper)
+# $ fisher install joseluisq/gitnow@2.5.1
