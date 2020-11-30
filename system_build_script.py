d8ce9822a55e6aaaac70e578c20aac03bc8c31cb
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-11-29 23:23:02 -0500

    add hotcorner disable for GNOME

--- system_build_script.py
+++ system_build_script.py
@@ -2,6 +2,15 @@
 print("WARNING:")
 print("THIS SCRIPT IS FOR BUILDING A NEW SYSTEM. ARE YOU SURE?")
 
+# GNOME STUFF
+
+# disable hotcorners:
+# $ gsettings set org.gnome.shell enable-hot-corners false
+
+
+
+# ANY DISTRO
+
 # install fish shell
 # $ curl -L https://get.oh-my.fish | fish
 
