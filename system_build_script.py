8d44f94f1524024fc7e5f82a80b88dad855a0a37
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-12-04 01:51:07 -0500

    add alacritty config and start system_build_script

--- system_build_script.py
+++ system_build_script.py
@@ -1,38 +1,75 @@
+#!/usr/bin/python
+from subprocess import run
+import sys
+import os
+
 # ARE YOU SURE YOU WANT TO DO THIS? Y/N
+print("#####################################################################")
 print("WARNING:")
 print("THIS SCRIPT IS FOR BUILDING A NEW SYSTEM. ARE YOU SURE?")
+print("#####################################################################")
+answer = input("Y/N? ")
+
+if answer.lower() != "y" and answer.lower() != "yes":
+    print("Ok bye")
+    sys.exit()
 
 # GNOME STUFF
 
 # disable hotcorners:
 # $ gsettings set org.gnome.shell enable-hot-corners false
 
+# ARCH
 
+# initial install
+install_list = [
+    "base-devel",
+    "wget", 
+    "tree", 
+    "git", 
+    "neovim", 
+    "tmux", 
+    "xclip",
+    "bat",
+    "fish",
+    "nodejs",
+    "npm"
+    # some deps for alacritty
+    "cmake", "freetype2", "fontconfig", "pkg-config", "make", "libxcb",
+    "alacritty",
+    "go", "go-tools",
+]
 
-# ANY DISTRO
+# unpack install_list with * to install 
+run(["sudo", "pacman", "-S", *install_list])
 
-# install wget tree git neovim tmux
+print("Installing Rust")
+run("wget https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init".split())
+run("chmod +x rustup-init".split())
+run("./rustup-init")
 
-# install rust
+# install yay
+print("cloning yay")
+run("git clone https://aur.archlinux.org/yay.git".split())
+os.chdir("yay")
+print("building yay")
+run("makepkg -si".split())
+print("leaving yay folder")
+os.chdir("..")
 
-# install bat, alacritty
+print("installing fisher, package manager for fish")
+run("yay -S fisher".split())
 
-# install fish shell
-# $ curl -L https://get.oh-my.fish | fish
-
-# install fisher, package manager for fish
-# $ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-# fisher plugs
+# fisher plugs (for some reason not working)
 
 # pisces
-# $ fisher install laughedelic/pisces
+# run(["fisher", "install", "laughedelic/pisces"])
 
 # z fish
-# $ fisher install jethrokuan/z
+# run("fisher install jethrokuan/z".split())
 
 # git status plugin
-# $ fisher install mattgreen/lucid.fish
+# run("fisher install mattgreen/lucid.fish".split())
 
 # gitnow (git helper)
-# $ fisher install joseluisq/gitnow@2.5.
+# run("fisher install joseluisq/gitnow@2.5.".split())
