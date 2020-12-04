3497523fe2f80fd520f7a10654e059f505bb200b
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-12-04 17:03:52 -0500

    -p on mkdir to silence the errors saying the folders exist already

--- backup.sh
+++ backup.sh
@@ -5,13 +5,13 @@ echo "copying ~/.tmux.conf"
 cp ~/.tmux.conf tmux.conf
 
 echo "copying ~/.config/nvim/init.vim" 
-mkdir ./config/nvim
+mkdir -p ./config/nvim
 cp ~/.config/nvim/init.vim ./config/nvim/
 
 echo "copying ~/.config/fish/config.fish"
-mkdir ./config/fish/
+mkdir -p ./config/fish/
 cp ~/.config/fish/config.fish ./config/fish/
 
 echo "copy ~/.config/alacritty/alacritty.yml"
-mkdir ./config/alacritty
+mkdir -p ./config/alacritty
 cp ~/.config/alacritty/alacritty.yml ./config/alacritty
