07b36e8418260a09d9a9fd0df48e372802d4376d
Ken Francis <kennethpatrickfrancis@gmail.com>
2020-12-09 20:17:44 -0500

    made propagate_dotfiles callable from the outside, cleaned up debian build script (hopefully working?)

---
+++ debian_built_script.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+from subprocess import run, call
+import sys
+import os
+import propagate_dotfiles
+
+# ARE YOU SURE YOU WANT TO DO THIS? Y/N
+print("#####################################################################")
+print("WARNING:")
+print("THIS SCRIPT IS FOR BUILDING A NEW SYSTEM. ARE YOU SURE?")
+print("#####################################################################")
+answer = input("Y/N? ")
+
+if answer.lower() != "y" and answer.lower() != "yes":
+    print("Ok bye")
+    sys.exit()
+
+
+# Debian 
+
+# TODO add community repos
+
+print("Setting Git identity")
+run('git config --global user.email "kennethpatrickfrancis@gmail.com"'.split())
+run('git config --global user.name "Ken Francis"'.split())  
+
+# initial install
+install_list = [
+#    "base-devel", TODO
+    "wget", 
+    "curl",
+    "tree", 
+    "git", 
+    "neovim", 
+    "tmux", 
+    "xclip",
+    "fish",
+    "nodejs",
+    "npm",
+    "cmake", "pkg-config", "make", 
+#    "go", "go-tools", # TODO
+    "python3", "python-pip", "ipython",
+#    "ncurses", # TODO
+    "jupyter-notebook",
+#    "texlive-most" # LaTeX # TODO
+]
+
+# unpack install_list with * to install 
+run(["sudo", "apt", "install", *install_list])
+
+# install vim-plug
+run(["curl", "-fLo", "/.local/share/nvim/site/autoload/plug.vim", "--create-dirs", "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"])
+
+### TODO add section generating ssh keys, w/ links and automation (raspberry pi, gitlab, github)
+print("doing ssh stuff.... NOT")
+
+print("Installing Rust")
+run("wget https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-gnu/rustup-init".split())
+run("chmod +x rustup-init".split())
+run("./rustup-init")
+call(". /home/ken/.cargo/env", shell=True)
+
+propagate_dotfiles.propagate()
+
+# GNOME STUFF
+
+# disable hotcorners:
+# $ gsettings set org.gnome.shell enable-hot-corners false
+
+# print("installing pip plugins")
+# run(["pip", "install", "-U", "gazpacho"])
+
+
+print("Installing Wasmer")
+call("curl https://get.wasmer.io -sSfL | sh", shell=True)
+call("sudo ln ~/.wasmer/bin/wasmer /usr/bin/wasmer", shell=True)
+
+print()
+print("#####################################################################")
+print("Please install fisher:")
+print("curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher")
+print("Copy/paste")
+print("#####################################################################")
+fisher_answer = input("Y/N? ")
+
+if fisher_answer.lower() != "y" and fisher_answer.lower() != "yes":
+    print("curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher")
+    print("Ok bye")
+    sys.exit()
+
+cargo_list = [
+    'bat',
+    'skim',
+    'starship',
+]
+
+run(["cargo", "install", *cargo_list])
+
+print("cleaning up")
+run("rm -rfv rust-init/".split())
+run("rm -fv rustup-init.1".split())
+run("rm -rfc __pycache__/".split())
+
+'''
+# pisces
+run(["fisher", "install", "laughedelic/pisces"])
+
+# z fish
+run(["fisher", "install", "jethrokuan/z"])
+
+# git status plugin
+run(["fisher", "install", "mattgreen/lucid.fish"])
+
+# gitnow (git helper)
+run(["fisher", "install", "joseluisq/gitnow@2.5.", "ojiry/fish-tmux", "oh-my-fish/plugin-bang-bang"])
+'''
--- propagate_dotfiles.py
+++ propagate_dotfiles.py
@@ -3,31 +3,37 @@ from subprocess import run
 import sys
 import os
 
-# ARE YOU SURE YOU WANT TO DO THIS? Y/N
-print("#####################################################################")
-print("WARNING:")
-print("You're about to overwrite the dotfiles on the system you're on. ARE YOU SURE?")
-print("#####################################################################")
-answer = input("Y/N? ")
+def propagate():
 
-if answer.lower() != "y" and answer.lower() != "yes":
-    print("Ok bye")
-    sys.exit()
+    # [FROM, TO], [FROM, TO]
+    config_list = [
+            ["config/nvim/init.vim", "/home/ken/.config/nvim/init.vim",],
+            ["config/fish/config.fish", "/home/ken/.config/fish/config.fish"],
+            ["tmux.conf", "/home/ken/.tmux.conf"],
+    ]
 
-# [FROM, TO], [FROM, TO]
-config_list = [
-        ["config/nvim/init.vim", "/home/ken/.config/nvim/init.vim",],
-        ["config/fish/config.fish", "/home/ken/.config/fish/config.fish"],
-        ["tmux.conf", "/home/ken/.tmux.conf"],
-]
+    # [FROM, TO], [FROM, TO]
+    arch_list = [
+        ["config/alacritty/alacritty.yml", "/home/ken/.config/alacritty/alacritty.yml"]
+    ]
+    
+    # do the actual copy
+    for i in range(len(config_list)):
+        print(f"copying {config_list[i][0]} to {config_list[i][1]}"), 
+        run(["cp", config_list[i][0], config_list[i][1],])
+    
 
-# [FROM, TO], [FROM, TO]
-arch_list = [
-    ["config/alacritty/alacritty.yml", "/home/ken/.config/alacritty/alacritty.yml"]
-]
+if __name__ == '__main__':
 
-# do the actual copy
-for i in range(len(config_list)):
-    print(f"copying {config_list[i][0]} to {config_list[i][1]}"), 
-    run(["cp", config_list[i][0], config_list[i][1],])
+    # ARE YOU SURE YOU WANT TO DO THIS? Y/N
+    print("#####################################################################")
+    print("WARNING:")
+    print("You're about to overwrite the dotfiles on the system you're on. ARE YOU SURE?")
+    print("#####################################################################")
+    answer = input("Y/N? ")
 
+    if answer.lower() != "y" and answer.lower() != "yes":
+        print("Ok bye")
+        sys.exit()
+
+    propagate()
